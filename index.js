83c9d599eba50a99e59d064eafaa45527230c422
Doreen <doreenmarie_cee@hotmail.com>
2023-10-14 00:41:22 +0200

    completing iteration 2

--- index.js
+++ index.js
@@ -17,7 +17,28 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    //Defining recipe details
+
+    const newRecipe = {
+      title: "Chocolate Billionaires",
+      level: "Easy Peasy",
+      ingredients: ["pecans", "chocolate chips", "rice crispies"],
+      cuisine: "choco cuisine",
+      dishType: "snacks",
+      image: "https://bakeitwithlove.com/wp-content/uploads/2022/12/Chocolate-Billionaires-sq.jpg.webp",
+      duration: 45,
+      creator: "Taste of Home",
+
+    };
+
+    return Recipe.create(newRecipe);
+
   })
+
+  .then ((recipe) => {
+    console.log(`New Recipe with title: ${recipe.title}`);
+  })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
